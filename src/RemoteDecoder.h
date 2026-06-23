d110b072226f9b66f5eee48f41c403c40aebcbc8
Yaser Rashnabadi <Yaser.Rashnabadi@outlook.com>
2026-06-23 17:31:44 +0330

    New 2026-06-23

--- src/RemoteDecoder.h
+++ src/RemoteDecoder.h
@@ -181,7 +181,7 @@ private:
 
    static bool isSyncPulse(uint16_t h, uint16_t l);
    static bool isBitZero(uint16_t h, uint16_t l);
-   static bool isBitOne(uint16_t h, uint16_t l);
+   static bool isBitOne(uint16_t h, uint16_t l);//
 
    static void resetBuffer(void);
    static void Decoder(void);
